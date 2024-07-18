c62bc409e45c40320a22675d4baf5a45f61200c7
Amit Dharmandra Chaurasiya <Amit.Chaurasiya@asl.mum.sst>
2024-07-18 10:04:51 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Developer Name is : {args[0]}");
+            Console.WriteLine($"Company Name is : {args[1]}");
+            Console.WriteLine($"Location is : {args[2]}");
+        }
+    }
+}
